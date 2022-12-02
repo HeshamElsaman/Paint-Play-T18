fac5f783a11bd2fb196dd5f7b46f84d93dd0cbf1
Abdelrahman Fouad Eldreeny <110930258+AbdelrahmanEldreeny@users.noreply.github.com>
2022-12-02 22:28:28 +0200

    Adding a Line
    Line operation and class was implemented

---
+++ Operations/opAddLINE.cpp
@@ -0,0 +1,55 @@
+#include "opAddLine.h"
+#include "..\shapes&Graph\Line.h"
+
+#include "..\controller.h"
+
+#include "..\GUI\GUI.h"
+
+opAddLINE::opAddLINE(controller * pCont):operation(pCont)
+{}
+opAddLINE::~opAddLINE()
+{} 
+
+//Execute the operation
+void opAddLINE::Execute() 
+{
+	
+	Point P1, P2;
+
+	//Get a Pointer to the Input / Output Interfaces
+	GUI* pUI = pControl->GetUI();
+
+	pUI->PrintMessage("New LINE: Click at first point");
+	//Read 1st point and store in point P1
+	pUI->GetPointClicked(P1.x, P1.y);
+
+	string msg = "First point is at (" + to_string(P1.x) + ", " + to_string(P1.y) + " )";
+	msg += " ... Click at second point";
+	pUI->PrintMessage(msg);
+	//Read 2nd point and store in point P2
+	pUI->GetPointClicked(P2.x, P2.y);
+	pUI->ClearStatusBar();
+
+	//Preapre all line parameters
+	GfxInfo LINEGfxInfo;
+	
+	//get drawing, filling colors and pen width from the interface
+	LINEGfxInfo.DrawClr = pUI->getCrntDrawColor();
+	LINEGfxInfo.FillClr = pUI->getCrntFillColor();
+	LINEGfxInfo.BorderWdth = pUI->getCrntPenWidth();
+
+
+	LINEGfxInfo.isFilled = false;	//default is not filled
+	LINEGfxInfo.isSelected = false;	//defualt is not selected
+
+
+	//Create a line with the above parameters
+	Line  *L=new Line(P1, P2, LINEGfxInfo);
+
+	//Get a pointer to the graph
+	Graph* pGr = pControl->getGraph();
+
+	//Add the line to the list of shapes
+	pGr->Addshape(L);
+
+}
---
+++ Operations/opAddLINE.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "operation.h"
+
+//Add Rectangle operation class
+class opAddLINE: public operation
+{
+public:
+	opAddLINE(controller *pCont);
+	virtual ~opAddLINE();
+	
+	//Add line to the controller
+	virtual void Execute() ;
+	
+};
+
